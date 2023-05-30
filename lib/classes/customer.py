b4b4b0e94270f47c734143a67e9e951b524061b5
kwamebrowne <theeecryptoking@gmail.com>
2023-05-30 01:16:31 -0500

    we dont lease steaks

--- lib/classes/customer.py
+++ lib/classes/customer.py
@@ -1,11 +1,38 @@
 class Customer:
     def __init__(self, name):
         self.name = name
-        
+        self._order = []
+        self._coffee = []
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <=15:
+            self._name = name
+        else:
+            raise Exception
+
     def orders(self, new_order=None):
         from classes.order import Order
-        pass
+        if new_order and isinstance(new_order, Order):
+            self._order.append(new_order)
+        else:
+            raise Exception
     
     def coffees(self, new_coffee=None):
         from classes.coffee import Coffee
-        pass
+        if new_coffee not in self._coffee and isinstance(new_coffee, Coffee):
+            self._coffee.append(new_coffee)
+        return self._coffee
+    
+    
+    
+    # def create_order(self):
+    #     return len(self._order)
+    
+     
+    # def average_price(self):
+    #     return sum([order.coffee for order in self._coffee]) / len(self._coffee)
--- lib/classes/order.py
+++ lib/classes/order.py
@@ -1,7 +1,35 @@
-
 class Order:
+    all = []
 
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+        coffee.orders(self)
+        coffee.customers(customer)
+
+        customer.orders(self)
+        customer.coffees(coffee)
+
+    @property
+    def customer(self):
+        return self._customer
+    
+    @customer.setter
+    def customer(self, customer):
+        from classes.customer import Customer
+
+        if isinstance(customer, Customer): self._customer = customer
+        else: raise Exception
+        
+    @property
+    def order(self):
+        return self._order
+    
+    @order.setter
+    def order(self, order):
+        from classes.order import Order
+        if isinstance(order, Order): self._order = order
+        else: raise Exception
