f3967e4b7bd9fd0410bc2ca7ac702674b5bf1267
kwamebrowne <theeecryptoking@gmail.com>
2023-05-30 13:11:58 -0500

    second attempt

--- lib/classes/coffee.py
+++ lib/classes/coffee.py
@@ -5,3 +5,36 @@ class Coffee:
         self._orders = []
         self._customers = []
 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not hasattr(self, 'name'):
+            self._name = name
+        else:
+            raise Exception 
+    
+    def orders(self, new_order=None):
+        from classes.order import Order 
+        if isinstance(new_order, Order):
+            self._orders.append(new_order)
+        return self._orders
+        pass
+
+    def customers(self, new_customer=None):
+        from classes.customer import Customer
+        # write unique list , meanisn not repeatin and all elements ORIGINAL
+        if new_customer not in self._customers and isinstance(new_customer, Customer):
+            self._customers.append(new_customer)
+        return self._customers
+        pass
+
+    def num_orders(self):
+        return len(self._orders)
+        pass
+
+    def average_price(self):
+        return sum([order.price for order in self._orders]) / len(self._orders)
+        pass
--- lib/classes/order.py
+++ lib/classes/order.py
@@ -1,6 +1,51 @@
 class Order:
+    all = []
 
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+        coffee.orders(self)
+        coffee.customers(customer)
+
+        customer.orders(self)
+        customer.coffees(coffee)
+
+    @property
+    def customer(self):
+        return self._customer
+    
+    @customer.setter
+    def customer(self, customer):
+        from classes.customer import Customer
+        if isinstance(customer, Customer):
+            self._customer = customer
+        else: 
+            raise Exception
+        
+    @property
+    def coffee(self):
+        return self._coffee
+    
+    @coffee.setter
+    def coffee(self, coffee):
+        from classes.coffee import Coffee
+        if isinstance(coffee, Coffee):
+            self._coffee = coffee
+        else:
+            raise Exception
+        
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, price):
+        if 1<= price <=10:
+            self._price = price
+        else:
+            raise Exception
+        
+        
