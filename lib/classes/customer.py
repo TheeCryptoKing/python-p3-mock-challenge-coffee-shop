640680fdd2af214011e9aa5dba1b7e92b6c3e858
kwamebrowne <theeecryptoking@gmail.com>
2023-05-30 12:17:12 -0500

    resetting main

--- lib/classes/customer.py
+++ lib/classes/customer.py
@@ -3,27 +3,3 @@ class Customer:
         self.name = name
         self._order = []
         self._coffee = []
-
-    @property
-    def name(self):
-        return self._name
-    
-    @name.setter
-    def name(self, name):
-        if isinstance(name, str) and 1 <= len(name) <=15:
-            self._name = name
-        else:
-            raise Exception("Name must be a string")
-
-    def orders(self, new_order=None):
-        from classes.order import Order
-        if new_order and isinstance(new_order, Order):
-            self._order.append(new_order)
-        return self._order 
-    
-    def coffees(self, new_coffee=None):
-        from classes.coffee import Coffee
-        if new_coffee not in self._coffee and isinstance(new_coffee, Coffee):
-            self._coffee.append(new_coffee)
-        return self._coffee
-    
